94cde9c5f5c6798415c881c6fdaa682ef6a00659
Maxim Penzin <maxp@ya.ru>
2014-05-22 01:12:39 +0900

    gms libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+//
+//  Angara.Net weather station sensor
+//
+//  DHT-22, BMP180, Wind vane, Anemometer, Arduino Leonardo, TinySine SIM900, IPS SKAT
+//  http://github.com/maxp/rs_arw
+//
+
+
+#define VERSION "rs_arw 0.2"
 
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
@@ -6,15 +15,26 @@
 Adafruit_BMP085 bmp;
 Dht dht;
 
-int bmp_ok = 0;
-
+#define BLINK_PIN 13
 #define DHT22_PIN 6
 
 #define VANE_PIN A0
 #define ANEM_PIN A1
+#define PWR_PIN  A2
+#define BAT_PIN  A3
 
 #define ANEM_INTERVAL 10
 #define ANEM_ALEVEL 500
+#define PWRBAT_ALEVEL 200
+
+#define HOST      "rs.angara.net"
+#define PORT       80
+#define BASE_URI  "/dat?"
+// #define SECRET    "$$$"
+
+#define APN   ""
+#define USER  ""
+#define PASS  ""
 
 // !!! 30
 #define SEC10_NUM 3
@@ -30,19 +50,43 @@ int RH_A[RHN] = {
 };
 #define RH_D 20
 
-
 int   cycle = 0;
 
 float t_sum,   h_sum,   p_sum,   w_sum,   t0_sum; 
 int   t_count, h_count, p_count, w_count, t0_count;
-int   gust, va_min, va_max;
-int   rh[RHN];
+int   gust, va_min, va_max, rh[RHN];
+
+
+// GSM.h:
+//   GSM_TX    2
+//   GSM_RX    3
+// GSM.cpp:
+//   GSM_RESET 9 (!!! TinySine sim900)
+//   GSM_ON    8 (!!! TinySine sim900)
+
+#include <SoftwareSerial.h>
+#include "SIM900.h"
+#include "inetGSM.h"
+// #include "sha1.h"
+
+InetGSM inet;
+
+#define IMEI_LEN 20
+
+char tchar[10];
+char imei[IMEI_LEN+1];
+
+char rbuff[50];
+char ubuff[120];
+
+//
 
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("rs_arw");
+  Serial.println(VERSION);
+  pinMode(BLINK_PIN, OUTPUT);
 }
 
 
@@ -63,13 +107,19 @@ int read_anem()
 {
     int count = 0;
     boolean high = false;
+    boolean blink = false;
     for(int seconds=0; seconds < ANEM_INTERVAL; seconds++ ) 
     {
+        digitalWrite(BLINK_PIN, HIGH);
         // one second cycle
-        for(int dm=0; dm < 100; dm++) {
+        for(int dm=0; dm < 100; dm++) 
+        {
             int v = analogRead(ANEM_PIN);
             if(v > ANEM_ALEVEL) {
-                if(!high){ high = true; }
+                if(!high){ 
+                    high = true; 
+                    digitalWrite(BLINK_PIN, blink?HIGH:LOW); blink = !blink;
+                }
             }
             else {
                 if(high) { high = false; count++; }
@@ -77,21 +127,83 @@ int read_anem()
             delay(10);
         }
     }
+    digitalWrite(BLINK_PIN, LOW);
     return count;
 }
 
 
-// read_bmp
+void sim_power() {
+  pinMode(GSM_ON, OUTPUT);
+  digitalWrite(GSM_ON, LOW);
+  delay(1000);
+  digitalWrite(GSM_ON, HIGH);
+  delay(1300);
+  digitalWrite(GSM_ON, LOW);
+  delay(2500);
+}
 
-// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
-// Connect GND to Ground
-// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
-// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
-// SCL -> D3, SDA -> D2 on Lennardo
+void sim_reset() {
+  pinMode(GSM_RESET, OUTPUT);
+  digitalWrite(GSM_RESET, LOW);
+  delay(100);
+  digitalWrite(GSM_RESET, HIGH);
+  delay(200);
+  digitalWrite(GSM_RESET, LOW);
+  delay(100);
+}
 
+// #include "sms.h"
+
+void gsm_send(char* buff) {
+  gsm.begin(9600);
+  
+  // SMSGSM sms;
+  // sms.SendSMS("+79025118669","test");
+  // sms.SendSMS("7070","GO");
+  
+  int rc;
+  rc = gsm.SendATCmdWaitResp("AT+GSN", 500, 100, "OK", 1);  // == 1
+  
+  int i = 0, j = 0; 
+  byte c; 
+  while((c = gsm.comm_buf[i]) && i < 1000) {
+    if( c <= 32 ) { i++; } else { break; }
+  }
+  while( (c = gsm.comm_buf[i]) && (j < IMEI_LEN) ) {
+    if( c <= 32 ) { break; }
+    imei[j++] = c; i++;
+  }
+  imei[j] = 0;
+  
+  Serial.print("\nimei: "); Serial.println(imei);
+ 
+  strcat(buff, "&hwid=");
+  strcat(buff, imei);
+  
+  // TODO: rbuff = sha1.update(ubuff).update(secret).hexdigest()
+  // strcat(ubuff, "&_hkey=");
+  // strcat(ubuff, rbuff);
+
+  inet.attachGPRS(APN, USER, PASS);
+  delay(2000);
+
+  gsm.SimpleWriteln("AT+CIFSR");
+  delay(5000);
+
+  Serial.print("\nurl: "); Serial.println(buff);
+  
+  inet.httpGET(HOST, PORT, buff, rbuff, 50); 
+  Serial.print("\nresp: "); Serial.println(rbuff);
+  
+  sim_reset();  
+}
+
+//
 
 void loop()
 {
+    char f[20];
+
     cycle++;
     Serial.print("cycle: "); Serial.println(cycle);
 
@@ -129,16 +241,35 @@ void loop()
             t0_sum += bmp.readTemperature(); t0_count++;
         }
 
+        Serial.print("pwr: "); Serial.println(analogRead(PWR_PIN));
+        Serial.print("bat: "); Serial.println(analogRead(BAT_PIN));
     }
 
-    if(t_count) { Serial.print("t: "); Serial.print(t_sum/t_count, 1); Serial.println(); }
-    if(h_count) { Serial.print("h: "); Serial.print(h_sum/h_count, 1); Serial.println(); }
-    if(p_count) { Serial.print("p: "); Serial.print(p_sum/p_count, 1); Serial.println(); }
-    if(t0_count){ Serial.print("t0: ");Serial.print(t0_sum/t0_count,1);Serial.println(); }
-    if(w_count) { Serial.print("w: "); Serial.print(w_sum/w_count, 1); Serial.println(); }
-    if(gust) { Serial.print("g: "); Serial.print(gust); Serial.println(); }
+    ubuff[0] = 0;
+    itoa(cycle, f, 10); strcat(ubuff, "cycle="); strcat(ubuff, f);
 
-    // send
+    if(t_count) { 
+        dtostrf(t_sum/t_count, 3, 1, f); strcat(ubuff, "&t="); strcat(ubuff, f);
+    }
+    if(h_count) { 
+        dtostrf(h_sum/h_count, 3, 1, f); strcat(ubuff, "&h="); strcat(ubuff, f);
+    }
+    if(p_count) { 
+        dtostrf(p_sum/p_count, 3, 1, f); strcat(ubuff, "&p="); strcat(ubuff, f);
+    }
+    if(t0_count){ 
+        dtostrf(t0_sum/t0_count, 3, 1, f); strcat(ubuff, "&t0="); strcat(ubuff, f);
+    }
+    if(w_count) { 
+        dtostrf(w_sum/w_count, 3, 1, f); strcat(ubuff, "&w="); strcat(ubuff, f);
+    }
+    if(gust) { 
+        dtostrf(float(gust), 1, 0, f); strcat(ubuff, "&g="); strcat(ubuff, f);
+    }
+
+    Serial.print("ubuff: "); Serial.println(ubuff);
+
+    // gsm_send(ubuff);
    
 }
 
---
+++ src/shematic.md
@@ -0,0 +1,30 @@
+
+
+
+Cable:
+
+    - 1. (green)  - vane 0      = gnd
+    - 2. (yellow) - anem 0      = gnd
+    - 3. (red)    - anem 1      = pin A1 / 5k +Vcc
+    - 4. (black)  - vane 1      = pin A0 / 5k +Vcc
+
+    - 5. (red)    - DHT22 Vcc   = +Vcc
+    - 6. (green)  - DHT22 data  = pin D6 / 10k +Vcc
+    - 7. (white)  - DHT22 gnd   = gnd
+    - 8. (blue)   - DHT22 gnd   = gnd
+
+
+BMP180:
+
+    SDA - D2
+    SDC - D3
+    GND - gnd
+    VIN - +3v
+
+SKAT:
+
+    1. (red)   = pull up 10k/10k - A2/A3
+    2. (blue)  = power ok (low)  - pin A2
+    3. (wblue) = bat ok (low)    - pin A3
+
+
